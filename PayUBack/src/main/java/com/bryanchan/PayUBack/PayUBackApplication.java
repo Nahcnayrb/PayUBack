185ebcad6f4509101fbb448dd2913c238a4fef58
bryan <bryanchan22tr@gmail.com>
2024-05-28 19:06:36 -0700

    Initial commit

---
+++ PayUBack/src/main/java/com/bryanchan/PayUBack/PayUBackApplication.java
@@ -0,0 +1,13 @@
+package com.bryanchan.PayUBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayUBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayUBackApplication.class, args);
+	}
+
+}
---
+++ PayUBack/src/test/java/com/bryanchan/PayUBack/PayUBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryanchan.PayUBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayUBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
